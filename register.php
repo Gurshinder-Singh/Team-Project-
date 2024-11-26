3ba8c191a23af73b58472486abfc81a9528d5304
Bilal Hussain <142759038+BilalH123@users.noreply.github.com>
2024-11-26 17:45:47 +0000

    Update register.php

--- register.php
+++ register.php
@@ -1,14 +1,15 @@
-<!-- Bilal section -->
+<!-- Bilal section, BASIC TEMPLATE -->
 <?php
 include 'db_config.php'
 if ($_SERVER["REQUEST_METHOD"]=="POST") {
 $username= $_POST['username'];
 $password= $_POST['password'];
+$email=$_POST['email'];
 
 
 $hashed_passoword= password_hash($password);
 
-$sql = "INSERT INTO users (username, password) VALUES ('$username', '$hashed_password')";
+$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashed_password')";
 
 
 if (mysqli_query($conn,$sql){
