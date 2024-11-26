5f11414e0c727df84bf80c13384072ba8b9cd4dd
Bilal Hussain <142759038+BilalH123@users.noreply.github.com>
2024-11-26 11:51:31 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,1 +1,19 @@
 <!-- Bilal section -->
+<?php
+include 'db_connect.php'
+if($_SERVER["REQUEST_METHOD"]=="POST") {
+$username= $_POST['Username'];
+$password= $_POST['Password'];
+
+
+if($password != $confirm_password){
+  echo "Password does not match";
+}
+else{
+  $hashed_passoword= password_hash($password);
+  
+
+
+
+
+?>
