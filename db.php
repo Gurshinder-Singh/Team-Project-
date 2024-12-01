589513e1d4f769fff75eda60e021fc1798bd61c7
Gurshinder-Singh <230209451@aston.ac.uk>
2024-12-01 10:37:30 +0000

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,16 @@
-<?php
 
-$db_host = 'localhost'; 
-$db_name = 'cs2team30_db'; 
-$username = 'cs2team30'; 
-$password = 'To9JV8nPTCYwpMh'; 
+<?php
+$host = "Localhost";
+$dbname = "cs2team30_db";
+$username = "cs2team30";
+$password = "To9JV8nPTCYwpMh";
 
 try {
-    $conn = new PDO("mysql:host=$db_host;dbname=$db_name", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
 } catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
 
