6627b186241efec8b54e00ddda23ced4dc98699f
Gurshinder-Singh <230209451@aston.ac.uk>
2024-12-01 13:23:15 +0000

    Update db.php

--- db.php
+++ db.php
@@ -8,7 +8,6 @@ $password = "To9JV8nPTCYwpMh";
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
