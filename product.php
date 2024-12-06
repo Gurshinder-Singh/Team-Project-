52add12ad0db95b49e7f133ec57191eec633f9fb
AbidaC <230109612@aston.ac.uk>
2024-12-06 23:10:30 +0000

    Update product.php

--- product.php
+++ product.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Luxus Product Catalogue</title>
-    <link rel="stylesheet" href="Stylesheet.css">
+    <link rel="stylesheet" href="stylesheet.css">
     <script defer src="script.js"></script>
 </head>
 
